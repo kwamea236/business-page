e2cfc9a908cd6959da5c9e14f83c6272ba376d5d
kwamea236 <kwamea236@protonmail.com>
2023-07-12 01:23:43 +0000

    initial commit

---
+++ source/_layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ $page->language ?? 'en' }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="canonical" href="{{ $page->getUrl() }}">
+        <meta name="description" content="{{ $page->description }}">
+        <title>{{ $page->title }}</title>
+        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
+    </head>
+    <body class="text-gray-900 font-sans antialiased">
+        @yield('body')
+    </body>
+</html>
